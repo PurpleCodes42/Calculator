2f76f7328abc5af42b6b69f26b3e0921a3349c09
Purple <purplecodes42@gmail.com>
2019-12-31 15:33:28 +0100

    even fancier now

--- Calculator.js
+++ Calculator.js
@@ -36,55 +36,83 @@ resultButton.addEventListener("click", displayResult);
 const clearButton = document.querySelector("#clear");
 clearButton.addEventListener("click", clearDisplay);
 
-/*
-function getLine() {
-    return line = document.getElementById("#line").textContent;
-}
-*/
+/* put numbers in display */
 
-/* display in line */
+function setNumbers(a){
+    currentDisplayValue += a;
+    line.textContent = line.textContent + a;
+  }
+
+/* display calculation*/
 
 function setCalc(a){
     displayValues.push(currentDisplayValue);
-    currentDisplayValue = "";
-    switch (a){
-        case "plus": 
-            a = " + ";
-            break;
-        case "minu": 
-            a = " - ";
-            break;
-        case "divi": 
-            a = " : ";
-            break;
-        case "mult": 
-            a = " x ";
-            break;
+    if (displayValues[displayValues.length-1] == ""){
+        window.alert("Please enter a number first!");
+        clearDisplay();
     }
+    else {
+        currentDisplayValue = "";
+        switch (a){
+            case "plus": 
+                a = " + ";
+                break;
+            case "minu": 
+                a = " - ";
+                break;
+            case "divi": 
+                a = " : ";
+                break;
+            case "mult": 
+                a = " x ";
+                break;
+        }
     displayValues.push(a);
     line.textContent = line.textContent + a;
+    }    
 }
 
-function setNumbers(a){
-  currentDisplayValue += a;
-  line.textContent = line.textContent + a;
-}
+/* calculate and display result */
 
 function displayResult() {
     displayValues.push(currentDisplayValue);
+    console.log(displayValues);
+    if (displayValues[displayValues.length-1] == ""){
+        window.alert("Please complete the calculation!");
+        displayValues.pop();
+    }
+    else {
     searchSpecificCalc(" x ");
     searchSpecificCalc(" : ");
     searchSpecificCalc(" + ");
     searchSpecificCalc(" - ");
-    line.textContent = line.textContent + " = " + displayValues[0];
+        if (isNaN(displayValues[0])) {
+            displayValues[0] = "Impossible operation"; 
+        }
+        else {
+            displayValues[0] = Math.roundBy(1, displayValues[0]);
+        }
+        line.textContent = line.textContent + " = " + displayValues[0];   
+    }
 }
+
+/* clear display */
+
+function clearDisplay(){
+    line.textContent = "";
+    currentDisplayValue = "";
+    calculations = [];
+    displayValues = [];
+}
+
+/* little helpers */
 
 function searchSpecificCalc(calcu){
     for (let i = 1; i < displayValues.length; i++){
         if (displayValues[i] == calcu){
-        result = operate(parseFloat(displayValues[i-1]), parseFloat(displayValues[i+1]), displayValues[i]);
-        displayValues.splice(i-1,3,result);
-        i = i - 1;
+            result = operate(parseFloat(displayValues[i-1]), parseFloat(displayValues[i+1]), displayValues[i]);
+            displayValues.splice(i-1,3,result);
+            i = i - 1; 
         }
     }   
 }
@@ -97,19 +125,22 @@ function operate(a, b, calcu){
         return a - b
     }
     if (calcu == " : "){
-        return a/b;
+        if (b == 0){
+            window.alert("Never divide by 0!");
+        }
+        else {
+            return a/b;
+        }
     }
     if (calcu == " x "){
         return a*b;
     }
 }
 
-function clearDisplay(){
-    line.textContent = "";
-    currentDisplayValue = "";
-    calculations = [];
-    displayValues = [];
-}
+Math.roundBy = function (decimalPlaces, n) {
+    var scale = Math.pow(10, decimalPlaces);
+    return Math.round(scale * n) / scale;
+};
 
 /*
 container.style.gridTemplateColumns = `repeat(4, 1fr)`;
