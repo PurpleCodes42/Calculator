08c606b0a8a7340c274edfb46a58965b5c905744
Purple <purplecodes42@gmail.com>
2020-01-02 13:53:40 +0100

    keyboard support on fleek

--- Calculator.js
+++ Calculator.js
@@ -1,6 +1,8 @@
 let currentDisplayValue = "";
 displayValues = new Array;
 calculations = new Array;
+calculationDone = false;
+pointSet = false;
 
 /* put event listeners on numbers*/
 
@@ -39,12 +41,31 @@ clearButton.addEventListener("click", clearDisplay);
 const clearLastValue = document.querySelector("#back");
 clearLastValue.addEventListener("click", clearValue);
 
-/* put numbers in display */
+const pointButton = document.querySelector("#point");
+pointButton.addEventListener(
+    "click", 
+    function() {
+        setNumbers(".");
+    }
+);
+
+/* put event listeners on keys */
+
+window.addEventListener('keydown', testAlert);
+
+/* put things in display */
 
 function setNumbers(a){
+    if (a == "." && pointSet){
+        return;
+    }
+    else {
     currentDisplayValue += a;
     line.textContent = line.textContent + a;
+    }
+    if (a == ".") pointSet = true;
   }
+
 
 /* display calculation*/
 
@@ -83,6 +104,9 @@ function displayResult() {
         window.alert("Please complete the calculation!");
         displayValues.pop();
     }
+    else if (calculationDone){
+        window.alert("Please clear the display first!")
+    }
     else {
     searchSpecificCalc(" x ");
     searchSpecificCalc(" : ");
@@ -96,15 +120,20 @@ function displayResult() {
         }
         line.textContent = line.textContent + " = " + displayValues[0];   
     }
+    calculationDone = true;
 }
 
 /* clear last value */
 
 function clearValue(){
+    if (calculationDone) {
+        window.alert("Please clear the display first!")
+    }
+    else {
     line.textContent = line.textContent.slice(0, line.textContent.length-1);
     currentDisplayValue = currentDisplayValue.slice(0, currentDisplayValue.length-1);
+    }
 }
-
 
 /* clear display */
 
@@ -113,7 +142,43 @@ function clearDisplay(){
     currentDisplayValue = "";
     calculations = [];
     displayValues = [];
+    pointSet = false;
+    calculationDone = false;
 }
+
+/* keyboard support */
+
+function testAlert() {
+    numberArray = [12, 33, 34, 35, 36, 37, 38, 39, 
+        40, 45, 48, 49, 50, 51, 52, 53, 54, 55, 56, 
+        57, 8, 13, 46, 190, 46, 188, 101, 107, 111, 
+        171, 173, 109, 106, 188];      
+    key = window.event.keyCode;
+    console.log(key);
+    for (let i = 0; i<= numberArray.length-1; i++){
+        if (numberArray[i] == key) {
+            if (key == 48 || key == 45) setNumbers(0);
+            else if (key == 48 || key == 45) setNumbers(0);
+            else if (key == 49 || key == 35) setNumbers(1);
+            else if (key == 50 || key == 40) setNumbers(2);
+            else if (key == 51 || key == 34) setNumbers(3);
+            else if (key == 52 || key == 37) setNumbers(4);
+            else if (key == 53 || key == 12) setNumbers(5);
+            else if (key == 54 || key == 39) setNumbers(6);
+            else if (key == 55 || key == 36) setNumbers(7);
+            else if (key == 56 || key == 38) setNumbers(8);
+            else if (key == 57 || key == 33) setNumbers(9);
+            else if (key == 190 || key == 46) setNumbers(".");
+            else if (key == 107 || key == 171) setCalc("plus");
+            else if (key == 173 || key == 109) setCalc("minu");
+            else if (key == 111) setCalc("divi");  
+            else if (key == 106 || key == 188) setCalc("mult");  
+            else if (key == 13) displayResult();     
+            else if (key == 8) clearValue();
+        }
+    }    
+}
+
 
 /* little helpers */
 
